cb9744ff70d3688de92f168b07702b837591f04a
IdanCohen <36813204+idan94@users.noreply.github.com>
2018-06-14 15:18:24 +0300

    new project

---
+++ cmake-build-debug/CMakeFiles/hw5_wet1.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hw5_wet1.dir/main.cpp.obj"
+  "hw5_wet1.pdb"
+  "hw5_wet1.exe"
+  "hw5_wet1.exe.manifest"
+  "libhw5_wet1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hw5_wet1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
